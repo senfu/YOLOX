5aedefba3e94eec90e006fee8b27c3a600d819b4
Junyan Li <lijunyan668@outlook.com>
2021-11-11 21:50:13 +0800

    fix

--- scripts/han5.sh
+++ scripts/han5.sh
@@ -1,1 +1,1 @@
-python tools/train.py -f exps/widerface/yolox_s.py -d 2 -b 64 --dist-url tcp://10.93.89.24:20809 --num_machines 4 --machine_rank 3
+python tools/train.py -f exps/widerface/yolox_s.py -b 64 --dist-url tcp://10.93.89.24:20809 --num_machines 4 --machine_rank 3
